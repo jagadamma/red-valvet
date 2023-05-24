b1a08fef393884ee2a6c13ea95add79303ced780
SagarPrateek1 <sagar.prateek@talocity.ai>
2023-05-24 11:47:04 +0530

    Schema fix

---
+++ core/schema.py
@@ -0,0 +1,72 @@
+SCHEMA1 = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "integer"
+    },
+    "name": {
+      "type": "string"
+    },
+    "website": {
+      "type": "string"
+    },
+    "type": {
+      "type": "string"
+    },
+    "industry": {
+      "type": "string"
+    },
+    "speciality": {
+      "type": "string"
+    },
+    "employee_count": {
+      "type": "integer"
+    },
+    "founded_in": {
+      "type": "string"
+    },
+    "description": {
+      "type": "string"
+    },
+    "linkedin_profile": {
+      "type": "string"
+    },
+    "facebook_profile": {
+      "type": "string"
+    },
+    "twitter_profile": {
+      "type": "string"
+    },
+    "youtube_profile": {
+      "type": "string"
+    },
+    "career_page_link": {
+      "type": "string"
+    },
+    "background_color": {
+      "type": "string"
+    },
+    "text_color": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "id",
+    "name",
+    "website",
+    "type",
+    "industry",
+    "speciality",
+    "employee_count",
+    "founded_in",
+    "description",
+    "linkedin_profile",
+    "facebook_profile",
+    "twitter_profile",
+    "youtube_profile",
+    "career_page_link",
+    "background_color",
+    "text_color"
+  ]
+}
